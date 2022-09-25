35bb5b0539a012359287a3fa59df4df608e812c7
Håkon Wardeberg <hakonwar@gmail.com>
2022-09-25 22:46:28 +0200

    Switch to vite

--- frontend-svelte/src/main.ts
+++ frontend-svelte/src/main.ts
@@ -1,7 +1,8 @@
+import "./app.css";
 import App from "./App.svelte";
 
 const app = new App({
-  target: document.body,
+  target: document.getElementById("app"),
   props: {
     server: "http://127.0.0.1:8080",
   },
