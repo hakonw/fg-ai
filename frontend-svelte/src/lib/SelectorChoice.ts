7017069a36a7a16fbf47d9b89b1ca75e32ca41fb
Håkon Wardeberg <haakon@adaptic.no>
2026-05-02 14:07:12 +0200

    merge inn v4

--- frontend-svelte/src/lib/SelectorChoice.ts
+++
@@ -1,9 +0,0 @@
-
-export type ChoiceT = {name: string, value: string};
-
-export let Choice = {
-    bestMatch: {name: "Best match", value: "bestMatch"},
-    sensitivity: {name: "Sensitivity", value: "sensitivity"},
-}
-
-export let Choices = [Choice.bestMatch, Choice.sensitivity]
