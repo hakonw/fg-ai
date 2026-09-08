e5c92a718a21c7b245484cd21859999c4ead3432
Håkon Wardeberg <haakon@adaptic.no>
2026-09-08 22:09:47 +0200

    correctly set cors

--- frontend/backend/main.py
+++ frontend/backend/main.py
@@ -33,7 +33,7 @@ ALLOW_CORS(
     origins=[
         "http://localhost:5173",
         "http://localhost:8080",
-        "https://hakonw.github.io/fg-ai/"
+        "https://hakonw.github.io"
     ],
 )
 
